8c251ac1dbf054120c686493bd4369f55aa14988
tomas.albanesi <tomas.albanesi2000@gmail.com>
2021-12-09 15:05:07 -0300

    Add project files.

---
+++ Example_EFCore_DBFirst/Models/Post.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Console_EFCore_DBFirst.Models
+{
+    public partial class Post
+    {
+        public int PostId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+    }
+}
---
+++ Example_EFCore_DBFirst/Program.cs
@@ -0,0 +1,33 @@
+﻿using Console_EFCore_DBFirst.Models;
+using System;
+using System.Linq;
+
+namespace Example_EFCore_DBFirst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new DB_EFCore_TestContext())
+            {
+                Console.WriteLine("Elementos antes de realizar el INSERT");
+                foreach (var post in context.Posts.ToList())
+                {
+                    Console.WriteLine(post.Title);
+                }
+
+                var p = new Post();
+                p.Title = "Las universidades volveran a la presencialidad";
+                p.Body = "Por lo que se esta hablando, en 2022 las universidades retomaran el ciclo de forma presencial";
+                context.Posts.Add(p); // es un INSERT INTO
+                context.SaveChanges(); // Guardar los cambios en la base de datos
+
+                Console.WriteLine("Elementos luego de realizar el INSERT");
+                foreach (var post in context.Posts.ToList())
+                {
+                    Console.WriteLine(post.Title);
+                }
+            }
+        }
+    }
+}
